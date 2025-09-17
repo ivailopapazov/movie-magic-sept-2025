a38f4e40da64fdf5d7b57afae70f162ee9114687
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-09-17 20:13:06 +0300

    Extract routes into separate module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import express from 'express'
+import express, { application } from 'express'
 import handlebars from 'express-handlebars'
-import homeController from './controllers/homeController.js';
-import movieController from './controllers/movieController.js';
-
+import routes from './routes.js';
 const app = express();
 
 // Setup Handlebars
@@ -17,8 +15,7 @@ app.set('views', 'src/views');
 app.use(express.static('src/public'));
 
 // Routes
-app.use(homeController);
-app.use(movieController);
+app.use(routes);
 
 // Start Server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+
+import { Router } from 'express';
+
+import homeController from './controllers/homeController.js';
+import movieController from './controllers/movieController.js';
+
+const routes = Router();
+
+routes.use(homeController);
+routes.use(movieController);
+
+export default routes;
