ef51dd3a5412a72d4995318ca64e6c4e14bb8198
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-09-17 21:11:47 +0300

    Add getOne movie service method

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -17,8 +17,9 @@ movieController.post('/create', (req, res) => {
 
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
+    const movie = movieService.getOne(movieId);
 
-    console.log(movieId)
+    console.log(movie)
 
     res.end();
 }); 
