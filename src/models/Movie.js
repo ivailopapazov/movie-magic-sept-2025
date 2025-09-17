11e033a55178f95a5ff521883f578c820ea9efe0
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-09-17 21:42:17 +0300

    Filter movies by year

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -50,6 +50,18 @@ export default class Movie {
             result = movies.filter(movie => movie._id === filter._id)
         }
 
+        if (filter.title) {
+            // TODO Search by title, partial match, case insensitive
+        }
+
+        if (filter.genre) {
+            // TODO Search by genre, exact match, case insensitive
+        }
+
+        if (filter.year) {
+            result = result.filter(movie => movie.year === filter.year);
+        }
+
         return result
     }
 
