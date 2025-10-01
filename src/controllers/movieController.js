2884d0c3729ffd877e5234f2e30115e2db2f826a
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-10-01 21:06:55 +0300

    Show buttons only for creators

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -29,9 +29,9 @@ movieController.get('/:movieId/details', async (req, res) => {
     // TODO Prepare view data (temp solution)
     const ratingViewData = '&#x2605;'.repeat(Math.trunc(movie.rating));
 
-    // const isCreator = 
+    const isCreator = req.user?.id && movie.creator == req.user.id;
 
-    res.render('movies/details', { movie, rating: ratingViewData });
+    res.render('movies/details', { movie, rating: ratingViewData, isCreator });
 });
 
 movieController.get('/search', async (req, res) => {
